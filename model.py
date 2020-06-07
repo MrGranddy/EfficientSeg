951fac34b21eb27ed306c39a866e7d08e891cb5d
MrGranddy <bugrayesilkaynak@gmail.com>
2020-06-07 04:35:52 +0300

    Built downsampling arm
    Successfully implemented encoding part of EffSeg also added stride == 0.5 option

--- model.py
+++ model.py
@@ -33,7 +33,11 @@ class MobileBlock(nn.Module):
             self._bn0 = nn.BatchNorm2d(num_features=oup, momentum=bn_mom, eps=bn_eps)
 
         # Depthwise convolution phase
-        self._depthwise_conv = nn.Conv2d(in_channels=oup, out_channels=oup, groups=oup, kernel_size=kernel_size, stride=stride, padding=1, bias=False)
+        if stride == 0.5:
+            self._depthwise_conv = nn.Conv2d(in_channels=oup, out_channels=oup, groups=oup, kernel_size=kernel_size, stride=1, padding=1, bias=False)
+        else:
+            self._depthwise_conv = nn.Conv2d(in_channels=oup, out_channels=oup, groups=oup, kernel_size=kernel_size, stride=stride, padding=1, bias=False)
+
         self._bn1 = nn.BatchNorm2d(num_features=oup, momentum=bn_mom, eps=bn_eps)
 
         # Squeeze and Excitation layer, if desired
@@ -47,6 +51,11 @@ class MobileBlock(nn.Module):
         self._project_conv = nn.Conv2d(in_channels=oup, out_channels=final_oup, kernel_size=1, bias=False)
         self._bn2 = nn.BatchNorm2d(num_features=final_oup, momentum=bn_mom, eps=bn_eps)
 
+        # If upsampling
+        if stride == 0.5:
+            self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+
+
         self.relu = nn.ReLU()
 
     def forward(self, inputs, drop_connect_rate=None):
@@ -58,6 +67,8 @@ class MobileBlock(nn.Module):
             x = self._bn0(x)
             x = self.relu(x)
 
+        if self.stride == 0.5:
+            x = self.upsample(x)
         x = self._depthwise_conv(x)
         x = self._bn1(x)
         x = self.relu(x)
@@ -76,17 +87,58 @@ class MobileBlock(nn.Module):
 
         # Skip connection and drop connect
         input_filters, output_filters = self.in_chn, self.out_chn
-        if self.id_skip and input_filters == output_filters:
+        if self.id_skip and input_filters == output_filters and self.stride == 1:
             # The combination of skip connection and drop connect brings about stochastic depth.
             if drop_connect_rate:
                 x = drop_connect(x, drop_connect_rate=drop_connect_rate, training=self.training)
             x = x + inputs  # skip connection
         return x
 
-block = MobileBlock(3, 64)
+class EfficientSeg(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.initial_channel_size = 64
+
+        self.stages = nn.ModuleList([])
+        self.stages.append(
+            nn.Sequential(
+                MobileBlock(3, self.initial_channel_size),
+                MobileBlock(self.initial_channel_size, self.initial_channel_size)
+            )
+        )
+        
+        for i in range(1,5):
+            in_chn = self.initial_channel_size * int(2**(i-1))
+            out_chn = self.initial_channel_size * int(2**i)
+            self.stages.append(
+                nn.Sequential(
+                    MobileBlock(in_chn, out_chn, stride=2, expand_ratio=6),
+                    MobileBlock(out_chn, out_chn, expand_ratio=6),
+                    MobileBlock(out_chn, out_chn, expand_ratio=6)
+                )
+            )
+
+
+    def forward(self, inputs):
+
+        x = inputs
+        outputs = []
+
+        for stage in self.stages:
+            x = stage(x)
+            outputs.append(x)
+
+        return outputs
+
+        
+
+block = EfficientSeg()
+
 
 inp = torch.rand(1,3,256,256)
 
 out = block(inp)
 
-print(out.shape)
+for o in out:
+    print(o.shape)
