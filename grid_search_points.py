54b844ccb29e49a707dc38f6abac99b600b8e472
MrGranddy <bugrayesilkaynak@gmail.com>
2020-07-18 00:40:57 +0300

    Reinit

--- grid_search_points.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-for a in np.arange(1, 2, 0.01):
-    for b in np.arange(1, 2**0.5, 0.01):
-        res = a * ( b ** 2 )
-        if abs(res - 2) < 0.001:
-            print("Depth: %.2f, Width: %.2f" % (a,b))
--- run.sh
+++
@@ -1,2 +0,0 @@
-python baseline.py --lr_init="1.e-3" --lr_min="0.00001" --batch_size="2" --lr_momentum="0.9" --lr_weight_decay="1.e-5" \
---dataset_mean 0.2870 0.3257 0.2854 --dataset_std 0.1879 0.1908 0.1880 --epochs="300" --num_workers="8"
